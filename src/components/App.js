6044611c00125770ac1e2d6d33912a0f36cacb8f
dac392 <dac392@scarletmail.rutgers.edu>
2022-08-27 15:21:41 -0400

    initial values

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import Header from './Header'
 import PlayerList from './PlayerList';
 import AddPlayerForm from './AddPlayerForm';
+import { ScoreBoardContext } from './Context';
 
 
 const App = ()=> {
+
+  const { actions } = useContext(ScoreBoardContext);
+  useEffect( ()=>{
+    actions.addPlayer("Guil");
+    actions.addPlayer("John");
+    actions.addPlayer("Jill");
+    actions.addPlayer("jake");
+  }, []);
+
   return (
       <div className="scoreboard">
         <Header title="Scoreboard" />
